6df6dd3a9c28f546decf3870be2ea9dc716d78a5
Elbarae <44276243+Elbarae1921@users.noreply.github.com>
2020-01-03 18:06:26 +0000

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include("inc/config.php");
-
-$query = mysqli_query($con, "SELECT * FROM `users` WHERE `email`='roadrollerda@gmail.com' AND `password`='diopassword'");
-
-echo mysqli_fetch_array($query)['email'];
-
-?>
